77b0cde764790a924a1203657db9b67e9a854e08
PythonMaster245 <connorlithgow@4hscream.org>
2026-09-18 16:09:17 -0500

    Added turret and launcher classes to help

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Launcher.java
@@ -0,0 +1,149 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.screamrobotics.SuperSCREAMLib.controller.PIDFController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Launcher{
+
+    public DcMotorEx launcherMotor;
+    public DcMotorEx launcherMotor2;
+    public double dblTargetVel = 0;
+    double maximum = 2700;
+    double changeThresholdPower = 0.01;
+    double changeThresholdVelocity = 3;
+    int velStorageSize = 3;
+    PIDFController launcherPIDF = new PIDFController(0.5,0,0,0.0004);
+    List<Double> velStorage = new ArrayList<>();
+    VoltageSensor voltageSensor;
+    double voltage;
+    double adjustedVelocity;
+    double power;
+    public static double proportional = 0.002;
+    double error;
+    double xSpeed;
+    double ySpeed;
+    Pose goalPose;
+    double distance;
+    double timeOfFlight;
+    double timeOfFlightMultiplier = 0.003;
+    Pose futurePose;
+    double newDistance;
+
+    public Launcher(DcMotorEx m_Launcher, DcMotorEx m_launcher2, VoltageSensor m_voltageSensor){
+        launcherMotor = m_Launcher;
+        launcherMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        launcherMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        launcherMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        launcherMotor2 = m_launcher2;
+        launcherMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        launcherMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        voltageSensor = m_voltageSensor;
+        //launcherMotor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        //launcherMotor.setVelocityPIDFCoefficients(9, 0.8, 0, 0.7);
+
+        for (int i = 0; i < velStorageSize; i++) {
+            velStorage.add(0.0);
+        }
+    }
+
+    public double calculatePower(double targetVelocity){
+        voltage = voltageSensor.getVoltage();
+        adjustedVelocity = (targetVelocity / (voltage / 12));
+        power = ((0.000380 * adjustedVelocity) + 0.057776);
+        error = targetVelocity - getVelocity();
+        power += (proportional * error);
+        return power;
+    }
+
+    public void setPower(double power){
+        if(Math.abs(power - launcherMotor.getPower()) > changeThresholdPower) {
+            launcherMotor.setPower(power);
+            launcherMotor2.setPower(power);
+        }
+    }
+
+    public void setVelocity(double m_velocity) {
+        if(m_velocity != getVelocity()) {
+            dblTargetVel = m_velocity;
+            setPower(calculatePower(dblTargetVel));
+            velStorage.add(getVelocity());
+            velStorage.remove(0);
+            //dblTargetVel = m_velocity;
+        }
+    }
+
+    public void setVelocitySimple(double m_velocity) {
+        if(Math.abs(m_velocity - launcherMotor.getVelocity()) > changeThresholdVelocity) {
+            launcherMotor.setVelocity(m_velocity);
+            launcherMotor2.setVelocity(m_velocity);
+        }
+    }
+
+    /*public void setRPM(double m_RPM) {
+        double m_RPMToVelocity = m_RPM / (6.28 * dblLaunchWheelRadius);
+        launcherMotorLeft.setVelocity(m_RPMToVelocity);
+    }*/
+
+    public double getVelocity(){
+        return launcherMotor.getVelocity();
+    }
+
+    /*public double getRPM() {
+        return launcherMotorLeft.getVelocity() * (6.28 * dblLaunchWheelRadius);
+    }*/
+
+    public void setLaunchVelocity(double m_Distance) {
+        double velocity = getLaunchVelocity(m_Distance);
+        setVelocity(velocity);
+    }
+
+    public double getLaunchVelocity(double m_Distance){
+        //return ((7.6943 * m_Distance) + 986.63);
+        return (0.0335 * Math.pow(m_Distance, 2)) + (0.8955 * m_Distance) + 1377.6;
+    }
+
+    public double getLaunchVelocityAuto(double m_Distance){
+        return ((7.6943 * m_Distance) + 986.63);
+        //return (0.0335 * Math.pow(m_Distance, 2)) + (0.8955 * m_Distance) + 1377.6;
+    }
+
+    public boolean isAtSpeed(){
+        double averageSpeed = 0;
+        int velStorageIndex = 0;
+
+        for (int i = 0; i < velStorageSize; i++) {
+            averageSpeed = averageSpeed + velStorage.get(velStorageIndex);
+            velStorageIndex ++;
+        }
+
+        averageSpeed = averageSpeed / velStorageSize;
+
+        return Math.abs((averageSpeed - dblTargetVel)) <= 20;
+    }
+
+    public double getDistance(GoBildaPinpointDriver pinpoint, Follower follower){
+        //Goal Pose logic here
+        xSpeed = pinpoint.getVelX(DistanceUnit.INCH);
+        ySpeed = pinpoint.getVelY(DistanceUnit.INCH);
+        distance = follower.pose().distance(goalPose);
+        timeOfFlight = distance * timeOfFlightMultiplier;
+        futurePose = new Pose((goalPose.x() - (xSpeed * timeOfFlight)), (goalPose.y() - (ySpeed * timeOfFlight)));
+        newDistance = follower.pose().distance(futurePose);
+        return newDistance;
+    }
+
+    public void setMaximum(double m_maximum){
+        maximum = m_maximum;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -0,0 +1,129 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.screamrobotics.SuperSCREAMLib.controller.PIDController;
+
+public class Turret {
+    Servo turretServoL;
+    Servo turretServoR;
+    public AnalogInput servoEncoder;
+    double turretServoPosition;
+    Pose goalPose;
+    double xSpeed;
+    double ySpeed;
+    double timeOfFlightMultiplier = /*0.048*/ 0.003;
+    double timeOfFlight/* = 0.048*/;
+    public double botHeading;
+    public double targetHeading;
+    double turretOffset;
+    double rotationLead;
+    double maxDegrees;
+    public double degreeNormalized;
+    public double degreeModulus;
+    public double pidOutputToServoPos;
+    public double kD = 0.0005;
+    public double kP = 0.005;
+    public static double kF = 0.1;
+    public static double feedForward = 0.1;
+    PIDController turretHeadingPID = new PIDController(kP, 0, kD);
+
+    public Turret(Servo m_turretServoL, Servo m_turretServoR, AnalogInput m_servoEncoder){
+        turretServoL = m_turretServoL;
+        turretServoR = m_turretServoR;
+        servoEncoder = m_servoEncoder;
+        //setPosition(0.5);
+    }
+
+    public void setPosition(double position){
+        turretServoL.setPosition(position);
+        turretServoR.setPosition(position);
+    }
+
+    public double convertDegToServoPos(double degree){
+        degreeNormalized = AngleUnit.normalizeDegrees(degree);
+        degreeModulus = degreeNormalized % 360;
+        if(degreeModulus < 0){
+            degreeModulus += 360;
+        }
+        if(degreeModulus < 5){
+            degreeModulus = 5;
+        }
+        if(degreeModulus > 350){
+            degreeModulus = 350;
+        }
+        //return ((0.002933 * degreeModulus) - 0.07);
+        return ((-0.002840 * degreeModulus) + 1.01666);
+    }
+
+    public double getTurretAngle(GoBildaPinpointDriver pinpoint, Follower follower){
+        //Goal Pose logic here
+        botHeading = pinpoint.getHeading(AngleUnit.DEGREES);
+        xSpeed = pinpoint.getVelX(DistanceUnit.INCH);
+        ySpeed = pinpoint.getVelY(DistanceUnit.INCH);
+        timeOfFlight = follower.pose().distance(goalPose) * timeOfFlightMultiplier;
+        targetHeading = Math.toDegrees(Math.atan2((goalPose.y() - follower.pose().y() - (ySpeed * timeOfFlight)), (goalPose.x() - follower.pose().x() - (xSpeed * timeOfFlight))));
+        turretOffset = targetHeading - botHeading;
+        //rotationLead = Math.toDegrees(follower.getAngularVelocity()) * timeOfFlight;
+        //turretOffset += rotationLead;
+        //turretOffset = ((turretOffset + 180) % 360) -180;
+        //turretOffset = Math.max(-maxDegrees, Math.min(maxDegrees, turretOffset));
+        return turretOffset;
+    }
+
+    public double getTurretAngle(GoBildaPinpointDriver pinpoint, Follower follower, Pose m_goalPose){
+        goalPose = m_goalPose;
+        botHeading = pinpoint.getHeading(AngleUnit.DEGREES);
+        xSpeed = pinpoint.getVelX(DistanceUnit.INCH);
+        ySpeed = pinpoint.getVelY(DistanceUnit.INCH);
+        timeOfFlight = follower.pose().distance(goalPose) * timeOfFlightMultiplier;
+        targetHeading = Math.toDegrees(Math.atan2((goalPose.y() - follower.pose().y() - (ySpeed * timeOfFlight)), (goalPose.x() - follower.pose().x() - (xSpeed * timeOfFlight))));
+        turretOffset = targetHeading - botHeading;
+        //rotationLead = Math.toDegrees(follower.getAngularVelocity()) * timeOfFlight;
+        //turretOffset += rotationLead;
+        //turretOffset = ((turretOffset + 180) % 360) -180;
+        //turretOffset = Math.max(-maxDegrees, Math.min(maxDegrees, turretOffset));
+        return turretOffset;
+    }
+
+    public void setPositionDeg(double positionDeg){
+        setPosition(convertDegToServoPos(positionDeg));
+        //turretServoR.setPosition(convertDegToServoPos(positionDeg));
+        turretServoPosition = convertDegToServoPos(positionDeg);
+    }
+
+    public void updatePosition(double headingDeg){
+        degreeNormalized = (AngleUnit.normalizeDegrees(headingDeg) + 360);
+        degreeModulus = degreeNormalized % 360;
+        if(degreeModulus < 5){
+            degreeModulus = 5;
+        }
+        if(degreeModulus > 350){
+            degreeModulus = 350;
+        }
+        double error = (degreeModulus - getPositionDegrees());
+        double pidOutput = turretHeadingPID.calculate(error);
+        pidOutputToServoPos = ((pidOutput + 1) / 2);
+        /*if(Math.abs(error) > 2){
+            pidOutputToServoPos -= feedForward * (error/Math.abs(error));
+        }*/
+        setPosition(pidOutputToServoPos);
+    }
+
+    public double getPositionDegrees(){
+        return ((-121.448569 * servoEncoder.getVoltage()) + 366.747416);
+    }
+
+    public boolean isAtPosition(GoBildaPinpointDriver pinpoint, Follower follower){
+        if(Math.abs(getTurretAngle(pinpoint, follower) - getPositionDegrees()) < 5){
+            return true;
+        }
+        return false;
+    }
+}
