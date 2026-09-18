5df18aca32e638106ca35343f5aec20c0e095829
imnothomeforsuppwr4 <anishav@4hscream.org>
2026-09-18 18:01:29 -0500

    April Tags v1.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class Intake {
+public class  Intake {
     Servo intakeServo;
     Servo intakeServo2;
     DcMotor intakeDcMotor;
