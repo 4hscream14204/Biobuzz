526ff0cfcfd46633aa8d8dcee2d787f9c7f2b728
Liams-The-Best-At-Redstone <lmverheecke@icloud.com>
2026-09-18 19:05:05 -0500

    added leave park auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Routes/ParkAndLeave.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Routes/ParkAndLeave.java
@@ -1,29 +1,36 @@
 package org.firstinspires.ftc.teamcode.pedro.Routes;
-
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
+import com.pedropathing.api.PoseFactory;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import static com.pedropathing.api.Paths.line;
 
 import org.firstinspires.ftc.teamcode.pedro.Constants;
+import org.screamrobotics.SuperSCREAMLib.command.CommandScheduler;
+import org.screamrobotics.SuperSCREAMLib.command.FollowPathCommand;
 
 @Autonomous
 public class ParkAndLeave extends OpMode {
+    private final PoseFactory p = PoseFactory.degrees();
+    private final Pose start = p.of(35, 8.5, 90);
+    private final Pose park = p.of(15, 105, 90);
     private Follower follower;
+
     @Override
     public void init(){
         follower = Constants.create(hardwareMap);
-
+        follower.setPose(start);
     }
 
     @Override
     public void start(){
-
+        CommandScheduler.getInstance().schedule(new FollowPathCommand(follower, line(start, park)));
     }
     @Override
     public void loop(){
-
+        follower.update();
+        CommandScheduler.getInstance().run();
     }
 }
