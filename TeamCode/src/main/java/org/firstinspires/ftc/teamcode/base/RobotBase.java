87eb61761db23ada3104bf4d50d5a4ecfeae9778
Liams-The-Best-At-Redstone <lmverheecke@icloud.com>
2026-09-19 17:19:28 -0500

    made chassis windmill and started robotbase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
@@ -1,12 +1,26 @@
 package org.firstinspires.ftc.teamcode.base;
 
-import org.firstinspires.ftc.teamcode.subsystems.Chassis;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
-import org.firstinspires.ftc.teamcode.subsystems.Windmill;
+import org.firstinspires.ftc.teamcode.subsystems.Launcher;
+import org.firstinspires.ftc.teamcode.subsystems.Chassis;
 
 public class RobotBase {
     Chassis chassisSubsystem;
     Intake intakeSubsystem;
-    Windmill windmillSubsystem;
-
+    //Windmill windmillSubsystem;
+    Launcher launcherSubsystem;
+    public RobotBase(HardwareMap hwMap) {
+        chassisSubsystem = new Chassis(
+                hwMap.dcMotor.get("leftMotor"),
+                hwMap.dcMotor.get("rightMotor")
+        );
+        intakeSubsystem = new Intake(
+                hwMap.dcMotor.get("intakeMotor"),
+                hwMap.servo.get("leftIntakeServo"),
+                hwMap.servo.get("rightIntakeServo")
+        );
+        
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Chassis.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 public class Chassis {
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    public Chassis(DcMotor m_leftDrive, DcMotor m_rightDrive) {
+        leftDrive = m_leftDrive;
+        rightDrive = m_rightDrive;
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void setPower(double power) {
+        leftDrive.setPower(power);
+        rightDrive.setPower(power);
+    }
 }
