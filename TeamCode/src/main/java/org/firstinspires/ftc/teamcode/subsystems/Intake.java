c4fd0c0913973b3cac914b88700d8d55fb517302
lauren <LaurenLehane@4hscream.org>
2026-09-19 17:07:52 -0500

    created launcher and intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Intake {
+    DcMotor intakeMotor;
+    Servo leftServo;
+    Servo rightServo;
+    public Intake(DcMotor m_intakeMotor, Servo m_leftServo, Servo m_rightServo){
+        intakeMotor = m_intakeMotor;
+        rightServo = m_rightServo;
+        leftServo = m_leftServo;
+    }
+    public void setPower(double power){
+        double servoPower = (power + 1)/2;
+        intakeMotor.setPower(power);
+        rightServo.setPosition(servoPower);
+        leftServo.setPosition(1 - servoPower);
+    }
 }
