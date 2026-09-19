c1051f289ee64f2f4040f882122108975ebb9276
lauren <LaurenLehane@4hscream.org>
2026-09-19 17:17:38 -0500

    created windmill

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Windmill.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Windmill.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Windmill {
+    DcMotor windmillMotor;
+    public Windmill(DcMotor m_windmillMotor){
+        windmillMotor = m_windmillMotor;
+    }
+    public void setPower(double power){
+        windmillMotor.setPower(power);
+    }
 }
