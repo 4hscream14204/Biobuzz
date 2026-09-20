ef626d09dcd9b447123032d1c8e0dcac90ca5dc0
TacoMaster635 <tuckerlithgow@4hscream.org>
2026-09-20 14:28:42 -0500

    worked onGarden auto for el win.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/GardenAutoForTheWin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/GardenAutoForTheWin.java
@@ -24,34 +24,40 @@ public class GardenAutoForTheWin extends OpMode {
     Path path3;
     Path path4;
     Path path5;
+    Path path6;
 
     PoseFactory poseFactory = PoseFactory.degrees();
-    Pose start = poseFactory.of(58.8,8.6,90);
-    Pose goToGarden = poseFactory.of(6.9, 8.1, 0);
-    Pose otherSide = poseFactory.of(46.5, 117, 270);
+    Pose start = poseFactory.of(55.2,8.1,180);
+    Pose goToGarden = poseFactory.of(13, 8.1, 180);
+    /*Pose otherSide = poseFactory.of(35, 117, 270);
     Pose otherSideControlPoint = poseFactory.of(2.29, 81.81, 270);
     Pose toTheFlower = poseFactory.of(53.2, 136.7, 0);
     Pose shoot = poseFactory.of(54, 125.9, 270);
     Pose park = poseFactory.of(6.2, 114.6, 90);
+    */
+
     @Override
     public void init() {
         CommandScheduler.getInstance().reset();
         follower = Constants.create(hardwareMap);
         follower.setPose(start);
         path = line(start, goToGarden).linear(start,goToGarden);
+        /*
         path2 = curve(goToGarden, otherSide, otherSideControlPoint).linear(goToGarden, otherSide);
         path3 = line(otherSide, toTheFlower).linear(otherSide, toTheFlower);
         path4 = line(toTheFlower, shoot).linear(toTheFlower, shoot);
         path5 = line(shoot, park).linear(shoot, park);
+        */
+
     }
     @Override
     public void start() {
         CommandScheduler.getInstance().schedule(
                 new FollowPathCommand(follower, path)
-               // ,new FollowPathCommand(follower, path2)
-                //,new FollowPathCommand(follower, path3)
-                //,new FollowPathCommand(follower, path4)
-                //,new FollowPathCommand(follower, path5)
+                /*new FollowPathCommand(follower, path2),
+                new FollowPathCommand(follower, path3),
+                new FollowPathCommand(follower, path4),
+                new FollowPathCommand(follower, path5)*/
         );
     }
     @Override
