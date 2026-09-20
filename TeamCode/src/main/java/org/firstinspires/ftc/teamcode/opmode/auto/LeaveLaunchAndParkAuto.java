b89dd8b271eb2506563ca1073ed14f473150dfcd
aizahr <aizrizwan16@gmail.com>
2026-09-20 16:13:28 -0500

    Edit flower auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/LeaveLaunchAndParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/LeaveLaunchAndParkAuto.java
@@ -21,12 +21,14 @@ public class LeaveLaunchAndParkAuto extends OpMode {
     Path path;
     Path path2;
     Path path3;
-
+Path path4;
     Pose start = poseFactory.of(55,8,180);
     //Pose launch =poseFactory.of(56,23,180);
-    Pose flowerOne = poseFactory.of(10,46,180);
-    Pose goingUnderHive = poseFactory.of(57,62,90);
+    Pose flowerOne = poseFactory.of(14,46,180);
+    Pose goingUnderHive = poseFactory.of(49,62,90);
     Pose goingUnderHiveCtrlPoint = poseFactory.of(57,31,90);
+    Pose positionToShoot = poseFactory.of(51,117,90);
+    Pose flowerTwo = poseFactory.of(35, 124,90);
 
 
 
@@ -35,16 +37,19 @@ public class LeaveLaunchAndParkAuto extends OpMode {
         CommandScheduler.getInstance().reset();
         follower = Constants.create(hardwareMap);
         path = line(start,flowerOne).constant(start);
-      //  path2 = curve(flowerOne,goingUnderHiveCtrlPoint,goingUnderHive).linear(flowerOne,goingUnderHive);
+        path2 = curve(flowerOne,goingUnderHiveCtrlPoint,goingUnderHive).linear(flowerOne,goingUnderHive);
+        path3 = line(goingUnderHive,positionToShoot).constant(goingUnderHive);
+        path4 = line(positionToShoot, flowerTwo).constant(goingUnderHive);
+
         follower.setPose(start);
     }
 
     @Override
     public void start(){
         CommandScheduler.getInstance().schedule(
-            //    new FollowPathCommand(follower, path),
-             //   new FollowPathCommand(follower, path2)
-            //    new FollowPathCommand(follower,path3)
+               new FollowPathCommand(follower, path),
+                new FollowPathCommand(follower, path2),
+               new FollowPathCommand(follower,path3)
 
         );
 
