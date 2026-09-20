b7902e413201d5409c2e33d90abfc34b5ffeb601
PythonMaster245 <connorlithgow@4hscream.org>
2026-09-20 16:38:46 -0500

    Updated transfer command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TransferCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TransferCommand.java
@@ -5,7 +5,9 @@ import com.pedropathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.base.RobotBase;
 import org.firstinspires.ftc.teamcode.subsystems.TransferBlocker;
 import org.screamrobotics.SuperSCREAMLib.command.InstantCommand;
+import org.screamrobotics.SuperSCREAMLib.command.ParallelCommandGroup;
 import org.screamrobotics.SuperSCREAMLib.command.SequentialCommandGroup;
+import org.screamrobotics.SuperSCREAMLib.command.WaitCommand;
 
 public class TransferCommand extends SequentialCommandGroup {
     RobotBase robotBase;
@@ -15,9 +17,12 @@ public class TransferCommand extends SequentialCommandGroup {
         follower = m_follower;
 
         addCommands(
-                new InstantCommand(() -> robotBase.intakeSubsystem.setPower(1)),
-                new LaunchCommand(robotBase, 1000),
-                new InstantCommand(() -> robotBase.tranferBlockersubsystem.setPosition(TransferBlocker.TransferBlockerPosition.RELEASE))
+                new InstantCommand(() -> robotBase.intakeSubsystem.setMotorPower(1)),
+                new InstantCommand(() -> robotBase.tranferBlockersubsystem.setPosition(TransferBlocker.TransferBlockerPosition.RELEASE)),
+                new WaitCommand(1000),
+                new ParallelCommandGroup(
+                        new InstantCommand(()->robotBase.tranferBlockersubsystem.setPosition(TransferBlocker.TransferBlockerPosition.STOP)),
+                        new InstantCommand(()->robotBase.intakeSubsystem.setMotorPower(0)))
         );
 
 
