03f6f8e7439abf6f7cdb47aede8efc1322e006bb
aizahr <aizrizwan16@gmail.com>
2026-09-21 19:30:05 -0500

    The flower auto worked!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/LeaveLaunchAndParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/LeaveLaunchAndParkAuto.java
@@ -13,6 +13,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.pedro.Constants;
 import org.screamrobotics.SuperSCREAMLib.command.CommandScheduler;
 import org.screamrobotics.SuperSCREAMLib.command.FollowPathCommand;
+import org.screamrobotics.SuperSCREAMLib.command.SequentialCommandGroup;
+import org.screamrobotics.SuperSCREAMLib.command.WaitCommand;
 
 @Autonomous (name = "FlowerAutoForTheWinnn")
 public class LeaveLaunchAndParkAuto extends OpMode {
@@ -22,34 +24,48 @@ public class LeaveLaunchAndParkAuto extends OpMode {
     Path path2;
     Path path3;
 Path path4;
+Path path5;
     Pose start = poseFactory.of(55,8,180);
     //Pose launch =poseFactory.of(56,23,180);
-    Pose flowerOne = poseFactory.of(16,46,180);
-    Pose goingUnderHive = poseFactory.of(49,62,90);
-    Pose goingUnderHiveCtrlPoint = poseFactory.of(57,31,90);
+    Pose flowerOne = poseFactory.of(16,43,180);
+    Pose goingUnderHive = poseFactory.of(56,38,90);
+
     Pose positionToShoot = poseFactory.of(51,117,90);
     Pose flowerTwo = poseFactory.of(35, 124,90);
+    Pose parking = poseFactory.of(8, 120, 90);
 
 
+    SequentialCommandGroup commandGroup;
 
     @Override
     public void init() {
         CommandScheduler.getInstance().reset();
         follower = Constants.create(hardwareMap);
-        path = line(start,flowerOne).constant(start);
-        path2 = curve(flowerOne,goingUnderHiveCtrlPoint,goingUnderHive).linear(flowerOne,goingUnderHive);
-        path3 = line(goingUnderHive,positionToShoot).constant(goingUnderHive);
-        path4 = line(positionToShoot, flowerTwo).constant(goingUnderHive);
+        path = line(start,flowerOne).linear(start,flowerOne);
+        path2 = line(flowerOne,goingUnderHive).linear(flowerOne,goingUnderHive);
+        path3 = line(goingUnderHive,positionToShoot).linear(goingUnderHive,positionToShoot);
+        path4 = line(positionToShoot, flowerTwo).linear(positionToShoot,flowerTwo);
+        path5 = line(flowerTwo,parking).linear(flowerTwo,parking);
 
         follower.setPose(start);
+
+        commandGroup = new SequentialCommandGroup(
+                new FollowPathCommand(follower, path),
+                new WaitCommand(1000),
+                new FollowPathCommand(follower, path2),
+                 new FollowPathCommand(follower,path3),
+                new WaitCommand(1000),
+                new FollowPathCommand(follower,path4),
+                new WaitCommand(1000),
+                new FollowPathCommand(follower,path5)
+
+        );
     }
 
     @Override
     public void start(){
         CommandScheduler.getInstance().schedule(
-               new FollowPathCommand(follower, path),
-                new FollowPathCommand(follower, path2)
-             //  new FollowPathCommand(follower,path3)
+               commandGroup
 
         );
 
